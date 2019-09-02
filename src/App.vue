da715cf12c23369cc4d4525135148c556fc2e83a
王莹 <leosmilence@163.com>
2019-09-02 12:13:20 +0800

    修改基本配置

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,17 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <a-form class="createleft" :form="form">
+    <a-form-item label="文本信息">
+      <a-input v-decorator="[
+      'username'
+      ]" />
+    </a-form-item>
+  </a-form>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
-
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class App extends Vue {}
-</script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+import { Vue, Component } from "vue-property-decorator";
+@Component({})
+export default class demo extends Vue {
+  form = this.$form.createForm(this);
 }
-</style>
+</script>
